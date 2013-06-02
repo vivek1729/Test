86a276a6ba371bcfbb2b1e3e8b45437057ebbda8
vivek1729 <sasuke.vivek@gmail.com>
2013-06-02 17:37:33 +0530

    Modified the php script

--- github.php
+++ github.php
@@ -1,3 +1,22 @@
 <?php
-	$output = shell_exec('git pull');
+   $outfile = tempnam(".", "cmd");
+   $errfile = tempnam(".", "cmd");
+   $descriptorspec = array(
+       0 => array("pipe", "r"),
+       1 => array("file", $outfile, "w"),
+       2 => array("file", $errfile, "w")
+   );
+   $proc = proc_open('git pull', $descriptorspec, $pipes);
+
+   if (!is_resource($proc)) return 255;
+
+   fclose($pipes[0]);    //Don't really want to give any input
+
+   $exit = proc_close($proc);
+   $stdout = file($outfile);
+   $stderr = file($errfile);
+
+   unlink($outfile);
+   unlink($errfile);
+
 ?>
