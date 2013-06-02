9bff8758371db7d86c83c1878c6b27995d64cb4b
vivek1729 <sasuke.vivek@gmail.com>
2013-06-02 21:04:59 +0530

    Testing webhook

---
+++ gittest.php
@@ -0,0 +1,25 @@
+<?php
+try
+        {
+                // Decode the payload json string
+                $payload = json_decode($_REQUEST['payload']);
+        }
+        catch(Exception $e)
+        {
+                exit(0);
+        }
+ 
+        // Pushed to master?
+        if ($payload->ref === 'refs/heads/master')
+        {
+                // Log the payload object
+                file_put_contents('logs.txt', print_r($payload, TRUE), FILE_APPEND);
+
+                // Write to file
+                $my_file = 'logs.txt';
+				$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+				$data = $payload;
+				fwrite($handle, $data);
+				 
+
+?>
