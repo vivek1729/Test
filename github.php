2bc2f37e1edf0e302296c02482f4b5b28a03ba34
vivek1729 <sasuke.vivek@gmail.com>
2013-06-02 23:12:18 +0530

    updated github.php

--- github.php
+++ github.php
@@ -1,6 +1,7 @@
 <?php
    $outfile = tempnam(".", "cmd");
    $errfile = tempnam(".", "cmd");
+   $payload = json_decode($_REQUEST['payload']);
    $descriptorspec = array(
        0 => array("pipe", "r"),
        1 => array("file", $outfile, "w"),
@@ -8,14 +9,14 @@
    );
    $proc = proc_open('git pull', $descriptorspec, $pipes);
 
-   if (!is_resource($proc)) return 255;
+   if (!is_resource($proc)) {file_put_contents('/var/chroot/home/content/97/11124597/html/projects/Test/logs.txt', $payload, FILE_APPEND);}
 
    fclose($pipes[0]);    //Don't really want to give any input
 
    $exit = proc_close($proc);
    $stdout = file($outfile);
    $stderr = file($errfile);
-
+   file_put_contents('/var/chroot/home/content/97/11124597/html/projects/Test/logs.txt', $e . ' ' . $payload, FILE_APPEND);
    unlink($outfile);
    unlink($errfile);
 
--- gittest.php
+++ gittest.php
@@ -20,6 +20,7 @@ try
 				$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
 				$data = $payload;
 				fwrite($handle, $data);
+        }
 				 
 
 ?>
